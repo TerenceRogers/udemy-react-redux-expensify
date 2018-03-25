1c9a06d234a745340710913994b0974fbaab12ee
Terence Rogers <trogers@tnlrogers.com>
2018-03-25 11:01:13 -0700

    Updated ExpensesSummary component based on training module feedback

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -4,12 +4,18 @@ import numeral from 'numeral';
 import getExpensesTotal from '../selectors/expenses-total';
 import getVisibleExpenses from '../selectors/expenses';
 
-export const ExpensesSummary = (props) => (
-  <div>
-    Viewing {props.expenseCount} expenses totaling&nbsp;
-    {numeral(props.expensesTotal/100).format('$0,0.00')}
-  </div>
-);
+export const ExpensesSummary = (props) => {
+  const expenseWord = props.expenseCount === 1 ? 'expense' : 'expenses';
+  const formattedExpenseTotal = numeral(props.expensesTotal/100).format('$0,0.00');
+  return (
+    <div>
+      <h1>
+        Viewing {props.expenseCount} {expenseWord}
+        &nbsp;totaling {formattedExpenseTotal}
+      </h1>
+    </div>
+  );
+};
 
 const mapStateToProps = (state) => {
   const expenses = getVisibleExpenses(state.expenses, state.filters);
--- src/tests/components/ExpensesSummary.test.js
+++ src/tests/components/ExpensesSummary.test.js
@@ -2,12 +2,12 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import { ExpensesSummary } from '../../components/ExpensesSummary';
 
-test('should render expenses summary with 0 expenses', () => {
-  const wrapper = shallow(<ExpensesSummary expenseCount={0} expensesTotal={0} />);
+test('should render ExpensesSummary correctly with 1 expense', () => {
+  const wrapper = shallow(<ExpensesSummary expenseCount={1} expensesTotal={1500} />);
   expect(wrapper).toMatchSnapshot();
 });
 
-test('should render expenses summmary with positive values', () => {
-  const wrapper = shallow(<ExpensesSummary expenseCount={5} expensesTotal={1234567} />);
+test('should render ExpenseSummary correctly with more than 1 expense', () => {
+  const wrapper = shallow(<ExpensesSummary expenseCount={3} expensesTotal={123456789} />);
   expect(wrapper).toMatchSnapshot();
 });
