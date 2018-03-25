b768403190275a1c3d11ad6329d84f98a3ff1b25
Terence Rogers <trogers@tnlrogers.com>
2018-03-24 19:40:42 -0700

    Modified config for prod support
    Changed webpack source-map to be different for prod vs dev; Split CSS files from the bundle.js file

--- src/app.js
+++ src/app.js
@@ -12,31 +12,6 @@ import './styles/styles.scss';
 
 const store = configureStore();
 
-store.dispatch(addExpense({
-  description: 'Gas Bill',
-  amount: 2500,
-  createdAt: 101,
-  note: 'For April 2018 bill',
-}));
-store.dispatch(addExpense({
-  description: 'Water Bill',
-  amount: 5000,
-  createdAt: 100,
-  note: 'For March 2018 bill',
-}));
-store.dispatch(addExpense({
-  description: 'Rent Bill',
-  amount: 125000,
-  createdAt: 102,
-  note: 'For March 2018 bill',
-}));
-console.log(store.getState());
-
-const state = store.getState();
-console.log(state.filters);
-
-console.log(getVisibleExpenses(state.expenses, state.filters));
-
 const jsx = (
   <Provider store={store}>
     <AppRouter />
